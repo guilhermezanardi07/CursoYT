0625b454f00fb126e7d11d99230c2fb582362efb
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-24 19:18:01 -0300

    Alteração feita no arquivo 'Card.tsx': Inserção de imagem.

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -7,6 +7,7 @@ const Card = (props: Props) => {
   return (
     <div className='card'>
         <div className='details'>
+            <img src="https://media.newyorker.com/photos/60df663b833fa66507c7515e/1:1/w_1679,h_1679,c_limit/Gopnik-Little-Liberty.jpg" alt="Estátua da liberdade" />
             <h2>AAPL</h2>
             <p>$110</p>
         </div>

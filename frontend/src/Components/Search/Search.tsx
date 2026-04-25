d7be4e2dc182784dddaa2c11389dce9b2c000fdc
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-25 15:06:00 -0300

    Modificação de conteúdo do arquivo 'Search.tsx': Inserção de conteúdo dentro da div, foi colocado um input cujo a value recebe 'search', evento 'onChange' inserido no input. Criação a const 'onClick'

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -4,9 +4,17 @@ type Props = {}
 
 const Search : React.FC<Props> = (props: Props) => {
     const [search, setSearch] = useState<string>("");
+
+    const onClick = (e: any) => {
+        setSearch(e.target.value);
+        console.log(e);
+    };
   return (
-    <div>Search</div>
-  )
+    <div>
+        <input value={search} onChange={(e) => onclick(e)}>
+        </input>
+    </div>
+  );
 };
 
 export default Search;

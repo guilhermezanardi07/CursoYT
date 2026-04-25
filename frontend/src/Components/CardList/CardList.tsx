30c2d2ced36a57b485cb9a3b9dcbc517d8778d9d
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-25 13:39:38 -0300

    Alteração feita no arquivo 'CardList.tsx': Inserção de informações nos Cards

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -6,9 +6,9 @@ interface Props {}
 const CardList = (props: Props) => {
   return (
     <div>
-        <Card />
-        <Card />
-        <Card />
+        <Card companyName="Apple" ticker='AAPL' price={100} />
+        <Card companyName="Microsoft" ticker='MSFT' price={200}  />
+        <Card companyName="Tesla" ticker='TSLA' price={300}  />
     </div>
   )
 }

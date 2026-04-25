033864f6c52aad5a8e0b42cd31c79c6386cefa93
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-25 13:47:42 -0300

    Alteração feita no arquivo 'Card.tsx': Modificação no conteúdo do 'h2' e do 'p', substituindo o conteúdo pelas propriedades.

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -12,8 +12,8 @@ const Card = ({companyName, ticker, price}: Props) => {
     <div className='card'>
         <div className='details'>
             <img src="https://media.newyorker.com/photos/60df663b833fa66507c7515e/1:1/w_1679,h_1679,c_limit/Gopnik-Little-Liberty.jpg" alt="Estátua da liberdade" />
-            <h2>AAPL</h2>
-            <p>$110</p>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
         </div>
         <p className='infon'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, laboriosam.</p>
     </div>

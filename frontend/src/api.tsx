f066d9b30350ae397ed73fe5ae903c65181ece48
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-25 17:26:40 -0300

    Correção de erros

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,23 +1,23 @@
-import axios from "axios"
-import { CompanySearch } from "./company"
+import axios from "axios";
+import { CompanySearch } from "./company";
 
 interface SearchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
 export const searchCompanies = async (query: string) => {
-    try {
-        const data = await axios.get<SearchResponse>(`https://api.twelvedata.com/symbol_search?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-        );
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://api.twelvedata.com/symbol_search?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
     return data;
-    } catch (error) {
-        if(axios.isAxiosError(error)) {
-            console.log("error message: ", error.message);
-            return error.message
-        } else {
-            console.log("unexpected error: ", error);
-            return "An expected error has occured.";
-        }
+  } catch (error: any) {
+    if (error && error.isAxiosError) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error has occurred.";
     }
-    
-}
+  }
+};

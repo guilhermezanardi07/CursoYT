f4ea67d470b3aa38be2e518ff0a592f188b139cf
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-26 16:03:16 -0300

    Arquivo 'CardList.tsx' atualizado: definida interface Props com searchResults e adicionada lógica de map com renderização condicional

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Card from '../Card/Card'
+import { CompanySearch } from '../../company'
 
-interface Props {}
+interface Props {
+  searchResults: CompanySearch[];
+}
 
-const CardList: React.FC<Props> = (props: Props) => {
-  return (
-    <div>
-        <Card companyName="Apple" ticker='AAPL' price={100} />
-        <Card companyName="Microsoft" ticker='MSFT' price={200}  />
-        <Card companyName="Tesla" ticker='TSLA' price={300}  />
-    </div>
-  )
+const CardList: React.FC<Props> = ({searchResults}: Props) => {
+  return <>
+  {searchResults.length > 0 ? (
+    searchResults.map((result) => {
+      result <Card id={result.symbol} key={}/>
+    })
+  ): (
+    <h1>No results</h1>
+  )}</>;
 }
 
 export default CardList

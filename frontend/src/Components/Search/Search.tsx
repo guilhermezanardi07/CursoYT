3cd0aa155266693b197ed458c52be01d76b607a2
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-26 17:18:05 -0300

    Arquivo 'Search.tsx' atualizado: implementado elemento form para suportar busca via Enter e renomeadas as props de evento

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,18 @@
 import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent, FormEvent } from 'react'
 
 interface Props {
-    onClick: (e: SyntheticEvent) => void;
+    onSearchSubmit: (e: SyntheticEvent) => void;
     search: string | undefined;
-    handleChange: (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => void;
+    handleSearchChange: (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => void;
 };
 
-const Search : React.FC<Props> = ({onClick, search, handleChange}: Props) => {
+const Search : React.FC<Props> = ({onSearchSubmit, search, handleSearchChange}: Props) => {
   return (
-    <div>
-        <input value={search} onChange={(e) => handleChange(e)}></input>
-        <button onClick={(e) => onClick(e)} />
-    </div>
+    <>
+      <form onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange} />
+      </form>
+    </>
   );
 };
 

a0b69df3e761b8bc3fb2e9fe9285420e48fbb2d3
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-26 14:16:08 -0300

    Adição de eventos em '<Search />'.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     };
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
       <CardList />
     </div>
   );

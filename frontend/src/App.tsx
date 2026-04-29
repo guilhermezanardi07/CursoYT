e21cb99e94c6db07d93386674d5b970b0d339be1
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-29 10:11:53 -0300

    Modificações nos arquivos Hero e Navbar e inserção da Navbar no App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Search from './Components/Search/Search';
 import { CompanySearch } from './company';
 import { searchCompanies } from './api';
 import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
+import Hero from './Components/Hero/Hero';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
     const [search, setSearch] = useState<string>("");
@@ -46,6 +48,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Search 
         onSearchSubmit={onSearchSubmit} 
         search={search} 

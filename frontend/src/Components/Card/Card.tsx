76d052eeec75feab1842adff5f435e3124f222bb
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-29 10:28:30 -0300

    Adição de estilização na página

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -11,16 +11,22 @@ interface Props {
 
 const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate }: Props) => {
   return (
-    <div className="card">
-      <img alt="company logo" />
-        <div className="details">
-            <h2>{searchResult.description} ({searchResult.displaySymbol})</h2>
-            <p>{searchResult.type}</p>
-        </div>
-        <p className="info">
-          {searchResult.symbol}
-        </p>
-        <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.displaySymbol}/>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.description} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.type}</p>
+      <p className="font-bold text-black">
+        {searchResult.displaySymbol}
+      </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   )
 }

28cb0597aa1cdaf186b4087982171a5da1017ba8
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-30 17:32:53 -0300

    Alteração feita em 'TenKFinder.tsx': criação do componente que busca e exibe os 5 filings 10-K mais recentes da empresa usando getTenK

--- frontend/src/Components/TenKFinder/TenKFinder.tsx
+++ frontend/src/Components/TenKFinder/TenKFinder.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CompanyTenK } from '../../company';
+import { getTenK } from '../../api';
 
-type Props = {}
+type Props = {
+    ticker: string;
+}
 
-const TenKFinder = (props: Props) => {
+const TenKFinder = ({ticker}: Props) => {
+    const [companyData, setCompanyData] = useState<CompanyTenK[]>();
+    useEffect(() => {
+        const getTenKData = async () => {
+            const value = await getTenK(ticker);
+            setCompanyData(value?.data);
+        };
+        getTenKData();
+    }, [ticker]);
   return (
-    <div>TenKFinder</div>
+    <div className='inline-flex rounded-md shadow-sm m-4'>
+        {companyData ? (
+            companyData?.slice(0, 5).map((tenK) => {
+                return <TenKFinderItem />
+            })
+        )}
+    </div>
   )
 }
 

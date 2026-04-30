3b0a7822e4a6d4980093e956a2313bf8ace4ae47
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-30 18:34:08 -0300

    adiciona formatação de números com formatLargeMonetaryNumber, formatLargeNonMonetaryNumber e formatRatio, e adapta CompanyProfile para usar as funções de formatação com campos da Finnhub

--- frontend/src/Components/CompanyProfile/CompanyProfile.tsx
+++ frontend/src/Components/CompanyProfile/CompanyProfile.tsx
@@ -4,58 +4,69 @@ import { useOutletContext } from 'react-router-dom';
 import { getKeyMetrics } from '../../api';
 import RatioList from '../RatioList/RatioList';
 import Spinner from '../Spinner/Spinner';
+import { formatLargeNonMonetaryNumber, formatRatio } from '../../Helpers/NumberFormatting';
 
 type Props = {}
 
 const tableConfig = [
   {
     label: "Market Cap",
-    render: (company: CompanyKeyMetrics) => company.metric.marketCapitalization,
+    render: (company: CompanyKeyMetrics) =>
+      formatLargeNonMonetaryNumber(company.metric.marketCapitalization),
     subTitle: "Total value of all a company's shares of stock",
   },
   {
     label: "Current Ratio",
-    render: (company: CompanyKeyMetrics) => company.metric.currentRatioTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.currentRatioTTM),
     subTitle: "Measures the companies ability to pay short term debt obligations",
   },
   {
     label: "Return On Equity",
-    render: (company: CompanyKeyMetrics) => company.metric.roeTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.roeTTM),
     subTitle: "Return on equity is the measure of a company's net income divided by its shareholder's equity",
   },
   {
     label: "Return On Assets",
-    render: (company: CompanyKeyMetrics) => company.metric.assetTurnoverTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.assetTurnoverTTM),
     subTitle: "Return on assets is the measure of how effective a company is using its assets",
   },
   {
     label: "Free Cashflow Per Share",
-    render: (company: CompanyKeyMetrics) => company.metric.cashFlowPerShareTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.cashFlowPerShareTTM),
     subTitle: "Return on assets is the measure of how effective a company is using its assets",
   },
   {
     label: "Book Value Per Share TTM",
-    render: (company: CompanyKeyMetrics) => company.metric.bookValuePerShareAnnual,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.bookValuePerShareAnnual),
     subTitle: "Book value per share indicates a firm's net asset value (total assets - total liabilities) on per share basis",
   },
   {
     label: "Dividend Yield TTM",
-    render: (company: CompanyKeyMetrics) => company.metric.currentDividendYieldTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.currentDividendYieldTTM),
     subTitle: "Shows how much a company pays each year relative to stock price",
   },
   {
     label: "Capex Per Share TTM",
-    render: (company: CompanyKeyMetrics) => company.metric.capexCagr5Y,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.capexCagr5Y),
     subTitle: "Capex is used by a company to aquire, upgrade, and maintain physical assets",
   },
   {
     label: "Graham Number",
-    render: (company: CompanyKeyMetrics) => company.metric.bookValuePerShareAnnual,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.bookValuePerShareAnnual),
     subTitle: "This is the upperbound of the price range that a defensive investor should pay for a stock",
   },
   {
     label: "PE Ratio",
-    render: (company: CompanyKeyMetrics) => company.metric.peBasicExclExtraTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.metric.peBasicExclExtraTTM),
     subTitle: "This is the upperbound of the price range that a defensive investor should pay for a stock",
   },
 ];

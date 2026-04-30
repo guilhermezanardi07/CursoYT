5b447e14f7d5811b600d52738707798259a4eb11
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-30 17:10:32 -0300

    Adição e importação do CompFinder na CompanyPage.tsx.

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -6,6 +6,7 @@ import Sidebar from '../../Components/Sidebar/Sidebar';
 import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
 import Tile from '../../Components/Tile/Tile';
 import Spinner from '../../Components/Spinner/Spinner';
+import CompFinder from '../../Components/CompFinder/CompFinder';
 
 interface Props {}
 
@@ -34,6 +35,7 @@ const CompanyPage = (props: Props) => {
             <Tile title="Industry" subTitle={company.finnhubIndustry} />
             <Tile title="Country" subTitle={company.country} />
             <Tile title="Market Cap" subTitle={company.marketCapitalization.toString()} />
+            <CompFinder ticker={company.ticker} />
             <p className='bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4'>
               {company.name} is a {company.country}-based company operating in the {company.finnhubIndustry} industry.
               {company.marketCapitalization

b67a18407cffd6e94141686412c34d0706f6cb44
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-04-30 18:47:24 -0300

    adiciona formatação monetária no BalanceSheet mantendo campos da Finnhub

--- frontend/src/Components/BalanceSheet/BalanceSheet.tsx
+++ frontend/src/Components/BalanceSheet/BalanceSheet.tsx
@@ -4,6 +4,7 @@ import { useOutletContext } from 'react-router';
 import { getBalanceSheet } from '../../api';
 import RatioList from '../RatioList/RatioList';
 import Spinner from '../Spinner/Spinner';
+import { formatLargeMonetaryNumber } from '../../Helpers/NumberFormatting';
 
 type Props = {}
 
@@ -11,52 +12,52 @@ const config = [
   {
     label: <div className="font-bold">Total Assets</div>,
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_Assets")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_Assets")?.value),
   },
   {
     label: "Current Assets",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_AssetsCurrent")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_AssetsCurrent")?.value),
   },
   {
     label: "Total Cash",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_CashAndCashEquivalentsAtCarryingValue")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_CashAndCashEquivalentsAtCarryingValue")?.value),
   },
   {
     label: "Property & Equipment",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_PropertyPlantAndEquipmentNet")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_PropertyPlantAndEquipmentNet")?.value),
   },
   {
     label: "Intangible Assets",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_FiniteLivedIntangibleAssetsNet")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_FiniteLivedIntangibleAssetsNet")?.value),
   },
   {
     label: "Long Term Debt",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_LongTermDebtNoncurrent")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_LongTermDebtNoncurrent")?.value),
   },
   {
     label: <div className="font-bold">Total Liabilities</div>,
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_Liabilities")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_Liabilities")?.value),
   },
   {
     label: "Current Liabilities",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_LiabilitiesCurrent")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_LiabilitiesCurrent")?.value),
   },
   {
     label: "Stakeholder's Equity",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_StockholdersEquity")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_StockholdersEquity")?.value),
   },
   {
     label: "Retained Earnings",
     render: (company: CompanyBalanceSheet) =>
-      company.report.bs.find(i => i.concept === "us-gaap_RetainedEarningsAccumulatedDeficit")?.value,
+      formatLargeMonetaryNumber(company.report.bs.find(i => i.concept === "us-gaap_RetainedEarningsAccumulatedDeficit")?.value),
   },
 ];
 

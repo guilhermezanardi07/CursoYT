c4b25d169c455aa9d7b155b44e2a5415225d41c1
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 17:31:50 -0300

    Configura injeção de dependência de IStockRepository e StockRepository em Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 var app = builder.Build();
 
@@ -55,7 +59,7 @@ app.MapControllers();
 
 app.Run();
 
-// ❌ também pode remover esse record se não for usar
+// também pode remover esse record se não for usar
 // record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 // {
 //     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);

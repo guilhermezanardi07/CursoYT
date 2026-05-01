3725e7176d20f3d3a5218adb13b6c8a13245e0a4
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 17:40:36 -0300

    Implementa métodos de CRUD em StockRepository conforme contrato de IStockRepository (ainda não implementados)

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +17,30 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public Task<Stock> CreateAsync(Stock stockModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }

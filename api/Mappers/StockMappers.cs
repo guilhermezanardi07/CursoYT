52468cd090b0063de4ca007a3138b023f721cf6a
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 14:53:17 -0300

    Implementa StockMappers com método ToStockDto para mapear Stock em StockDto

--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using api.Dtos.Stock;
+using api.Models;
 
 namespace api.Mappers
 {
-    public class StockMappers
+    public static class StockMappers
     {
-        
+        public static StockDto ToStockDto(Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
     }
 }

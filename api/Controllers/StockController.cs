88c40ac6cfe8a1fe998026ded582c5200328729b
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 16:37:46 -0300

    Altera GetById em StockController para versão assíncrona com FindAsync

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -33,9 +33,9 @@ namespace api.Controllers
 
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = _context.Stocks.Find(id);
+            var stock = await _context.Stocks.FindAsync(id);
 
             if (stock == null)
             {

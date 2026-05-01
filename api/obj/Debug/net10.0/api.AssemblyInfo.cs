f8d86c951e6059706aa6520021e5857541671bfa
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 15:20:21 -0300

    Criação do 'CreateStockRequestDto.cs'

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fe82da0d5bc3924c339733d7a59b8a4a44fee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f29ae8900bb1110c14fc0292a309ccd1db6d50b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

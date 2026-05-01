fcaa9418322625abb13b048adb531bea8d128e49
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 11:20:00 -0300

    configuração do Entity Framework com DbContext e integração com banco de dados PostgreSQL utilizando Npgsql

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,16 @@
-/*var builder = WebApplication.CreateBuilder(args);
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); 
+});
 
 var app = builder.Build();
 
@@ -38,7 +46,9 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}*/
+}
+
+/*
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -58,3 +68,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+*/

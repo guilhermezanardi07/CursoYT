1e98dca968b6adf88c9668d70ea17dcfbcf371c2
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 17:18:54 -0300

    Adiciona injeção de dependência de IStockRepository em StockController

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using api.Mappers;
 using api.Dtos.Stock;
 using Microsoft.EntityFrameworkCore;
+using api.Interfaces;
 
 
 namespace api.Controllers
@@ -16,8 +17,10 @@ namespace api.Controllers
     public class StockController : ControllerBase
     {
         private readonly ApplicationDBContext _context;
-        public StockController(ApplicationDBContext context)
+        private readonly IStockRepository _stockRepo;
+        public StockController(ApplicationDBContext context, IStockRepository stockRepo)
         {
+            _stockRepo = stockRepo;
             _context = context;
         }
 

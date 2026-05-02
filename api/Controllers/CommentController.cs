2bc85a7253c0f61772fb1aa18de56338a5fe79b0
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 23:36:46 -0300

    Implementa endpoint Delete em CommentController integrando CommentRepository

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -74,7 +74,14 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            
+            var commentModel = await _commentRepo.DeleteAsync(id);
+
+            if (commentModel == null)
+            {
+                return NotFound("Comment does not exist");
+            }
+
+            return Ok(commentModel);
         }
     }
 }

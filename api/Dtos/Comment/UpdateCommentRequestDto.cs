2f7dd39576646dfffbe0b0e7c253dd5237e537ba
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 23:08:28 -0300

    Cria DTO UpdateCommentRequestDto para atualização de comentários

--- api/Dtos/Comment/UpdateCommentRequestDto.cs
+++ api/Dtos/Comment/UpdateCommentRequestDto.cs
@@ -7,6 +7,7 @@ namespace api.Dtos.Comment
 {
     public class UpdateCommentRequestDto
     {
-        
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
     }
 }

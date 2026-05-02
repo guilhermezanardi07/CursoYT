a6f3c1c7f2bbab315c2d7a12700a9dac7e3ab54d
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 13:51:15 -0300

    Criação do diretório 'Account' no diretório 'Dtos'. Arquivo 'RegisterDto.cs' criado no diretório 'Account'.

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713add67388f88e9019ba01d9cc70f4fcadb51cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3f430964361c5c20d61310fbeaec08e67c77b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

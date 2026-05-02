5edf47d57200410b49d0f2958e60e08662008a7d
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 16:24:50 -0300

    Criação do arquivo 'PortfolioController.cs'. Cria estrutura inicial do PortfolioController com injeção de UserManager e IStockRepository

---
+++ api/Controllers/PortfolioController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace api.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepo;
+        public PortfolioController(UserManager<AppUser> userManager, 
+        IStockRepository stockRepo)
+        {
+            _userManager = userManager;
+            _stockRepo = stockRepo;
+        }
+    }
+}

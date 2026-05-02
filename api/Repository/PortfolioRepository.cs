93f1dccb964127fa9f5410bde68838356c667a68
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 17:05:29 -0300

    Altera PortfolioRepository para injetar ApplicationDBContext via construtor e preparar acesso ao banco

--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
 using api.Interfaces;
 using api.Models;
 
@@ -9,6 +10,11 @@ namespace api.Repository
 {
     public class PortfolioRepository : IPortfolioRepository
     {
+        private readonly ApplicationDBContext _context;
+        public PortfolioRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
         public Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             throw new NotImplementedException();

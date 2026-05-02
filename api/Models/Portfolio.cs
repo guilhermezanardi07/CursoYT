1efcc6fbbd52cc887493bd7258c2ce6bee58982c
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 16:05:41 -0300

    Adiciona anotação [Table("Portfolios")] em Portfolio para mapear explicitamente a tabela no banco

--- api/Models/Portfolio.cs
+++ api/Models/Portfolio.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.Models
 {
+    [Table("Portfolios")]
     public class Portfolio
     {
         public string AppUserId { get; set; }

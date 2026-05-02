1497dc8009ae73abd117cf47a1ac43650ef061df
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 23:19:47 -0300

    Corrige CommentMapper removendo atribuição de CreatedOn em ToCommentFromUpdate

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -32,5 +32,14 @@ namespace api.Mappers
             };
         }
 
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto commentDto)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+            };
+        }
+
     }
 }

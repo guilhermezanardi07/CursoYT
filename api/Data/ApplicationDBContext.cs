e37ad4c7ae73d98fa61e1fc80227094ca1f4d001
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 12:56:58 -0300

    Configura ApplicationDBContext para suportar identidade com AppUser e salvar CreatedOn em UTC

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)

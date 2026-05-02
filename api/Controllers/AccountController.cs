162e30b5279ff8204c836d9cd1ccb3b5aee9cca7
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 14:10:30 -0300

    Corrige AccountController: fecha parêntese em Register e ajusta nome da variável UserManager

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Account;
 using api.Models;
@@ -13,14 +11,15 @@ namespace api.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly UserManager<AppUser> _useManager;
+        private readonly UserManager<AppUser> _userManager;
+
         public AccountController(UserManager<AppUser> userManager)
         {
-            _useManager = userManager;
+            _userManager = userManager;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
             try
             {
@@ -33,11 +32,11 @@ namespace api.Controllers
                     Email = registerDto.Email,
                 };
 
-                var createdUser = await _useManager.CreateAsync(appUser, registerDto.Password);
+                var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
 
                 if (createdUser.Succeeded)
                 {
-                    var roleResult = await _useManager.AddToRoleAsync(appUser, "User");
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded)
                     {
                         return Ok("User created");
@@ -47,10 +46,14 @@ namespace api.Controllers
                         return StatusCode(500, roleResult.Errors);
                     }
                 }
-            } 
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
             catch (Exception e)
             {
-                
+                return StatusCode(500, e);
             }
         }
     }

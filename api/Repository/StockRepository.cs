2c59c7b1bc9494a919b651ee03f7c01332fedbd5
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-01 22:24:16 -0300

    Corrige método StockExists em StockRepository para usar AnyAsync em vez de AddAsync

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
@@ -46,7 +44,14 @@ namespace api.Repository
 
         public async Task<Stock?> GetByIdAsync(int id)
         {
-            return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(i => i.Id == id);
+            return await _context.Stocks
+                .Include(c => c.Comments)
+                .FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<bool> StockExists(int id)
+        {
+            return await _context.Stocks.AnyAsync(s => s.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)

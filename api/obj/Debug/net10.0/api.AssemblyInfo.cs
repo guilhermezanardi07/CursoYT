eec46938793bcb67a4210ebe9807f4671f8ecaeb
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 16:13:10 -0300

    Delete do diretório Migrations e seus arquivos.

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d87f6caa7767435fb65ef1e99a95506e77b62773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602704318b1f89909081dec15a53b35eba5e9a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

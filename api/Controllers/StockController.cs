d87f6caa7767435fb65ef1e99a95506e77b62773
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 15:54:05 -0300

    Implementa StockController com endpoints CRUD, integração com IStockRepository e proteção de listagem com [Authorize]

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -9,6 +9,7 @@ using api.Dtos.Stock;
 using Microsoft.EntityFrameworkCore;
 using api.Interfaces;
 using api.Helpers;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace api.Controllers
@@ -26,6 +27,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if(!ModelState.IsValid)

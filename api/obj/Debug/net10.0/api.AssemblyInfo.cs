3530d13e4d8bed0665237abf4f26ab3630efca53
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 00:37:19 -0300

    Atualização de artefatos de build gerados pelo .NET

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb6fd0667346a6dd1a94715dcb4a14a4270981b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace2a75895b0657a7964f16d8bbd810068dd1e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4f28ec0064d96d7280c8b5aee732ae79fa716f4e
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 12:51:05 -0300

    Adiciona pacotes de autenticação e identidade (Identity Core, EntityFrameworkCore e JwtBearer) ao projeto

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b46c48b7d88962bb8dd8662c0c18c774be0aa97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b32f0566584af8f73a033268e090664ff10623")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

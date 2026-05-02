c4a2e16fbf3fe2c8521aae91d87793964ae83a85
guilhermezanardi07 <guilherme.zanardi07@gmail.com>
2026-05-02 15:18:31 -0300

    Implementa DTO LoginDto com propriedades obrigatórias de usuário e senha para autenticação

--- api/Dtos/Account/LoginDto.cs
+++ api/Dtos/Account/LoginDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace api.Dtos.Account
 {
     public class LoginDto
     {
-        
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }
